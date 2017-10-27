990c31281fae6bb5c5faa8479e4b56452de9aeeb
Sam Abbott <signin@samabbott.co.uk>
2017-10-27 10:09:08 +0000

    improved language in detail on TB interventions

--- data-raw/tb_interventions_timeline.R
+++ data-raw/tb_interventions_timeline.R
@@ -35,7 +35,7 @@ tb_interventions_timeline <- data_frame(year = c(1921, 1944, 1944, 1952, 1952, 1
                                            "",
                                            "",
                                            "Second"),
-                                  detail = c("The first use of the Bacillus Calmette–Guérin (BCG) vaccine in humans, it remains the only vaccine against Tuberculosis (tb). 
+                                  detail = c("The first use of the Bacillus Calmette–Guérin (BCG) vaccine in humans, it remains the only vaccine against Tuberculosis (TB). 
                                               Efficacy has been shown to vary depending on latitude (0-80%) and there is only strong evidence of protection for 
                                              10-15 years after vaccination.",
                                             "The first antibiotic and the first bacterial agent against TB.",
@@ -46,7 +46,7 @@ tb_interventions_timeline <- data_frame(year = c(1921, 1944, 1944, 1952, 1952, 1
                                              "An antibiotic with severe side effects such as kidney failure and neurological conditions, which is therefore restricted for use against multiple
                                              drug resistant Tuberculosis.",
                                              "After a successful trial, which showed high effectiveness
-                                             for the vaccine, BCG was introduced for those at school leaving age
+                                             for the vaccine, BCG was introduced in the UK for those at school leaving age
                                              as peak incidence was then in young, working-adults.",
                                              "Believed to work by interfering with TB bacteria's metabolism. There are some concerns that it may not be safe to give during pregancy,
                                              as it may lead to vision loss in the baby.",
@@ -57,8 +57,8 @@ tb_interventions_timeline <- data_frame(year = c(1921, 1944, 1944, 1952, 1952, 1
                                              "Due to a continued decline in TB incidence rates in the indigenous UK population, the BCG 
                                              programme was refocused as risk-based. This meant vaccinating high risk neonates rather than those 
                                              most likely to transmit TB.",
-                                             "The first new antiobiotic for use against TB in 40 years, reserved for use against multiple drug resistant TB. Approved via a fast track process there
-                                             is still significant concern due to higher mortality in those that recieve the antibiotic."))
+                                             "The first new antiobiotic for use against TB in 40 years, reserved for use against multiple drug resistant TB. Approved via a fast track process, higher mortality in those that recieve the antibiotic has caused
+                                             significant concern."))
 
 
 
